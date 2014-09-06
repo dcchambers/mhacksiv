c68e31fe461e82d52967b93afdf05b0b61f12a84
Dakota Chambers <dakotachambers@gmail.com>
2014-09-06 14:20:22 -0500

    fixed grammar
    fixed grammar in the random string gen, on to formatting!

--- hackgen.js
+++ hackgen.js
@@ -3,7 +3,7 @@
 
 var stringOne = ["Virtual Reality", "Cloud-based", "Open Source",
  "Gamified", "Adaptive", "Social", "Distuptive", "Automated", "Back-end", "Front-end",
- "Collaborative", "Scalable"];
+ "Collaborative", "Scalable", "Educational", ];
 var stringTwo = ["Social Networking", "Enterprise", "Android", "iOS",
  "Windows", "Mac", "Linux", "Windows Phone", "Web", "Artificial Intelligence", "Gen-X"
  , "Big Data", "Solution", "E-commerce", "Big Data", "Networking"];
@@ -12,6 +12,15 @@ function generateRands() {
 
 	var tempOne = stringOne[Math.floor((Math.random() * stringOne.length))];
 	var tempTwo = stringTwo[Math.floor((Math.random() * stringTwo.length))];
+
+	var firstChar = tempOne.charAt(0);
+	if (firstChar == ('A') || firstChar == ('E') || firstChar == ('I') || 
+		firstChar == ('O') || firstChar == ('U')){
+		document.getElementById("aORan").innerHTML = "an";
+	}
+	else {
+		document.getElementById("aORan").innerHTML = "a";
+	}
 
 	document.getElementById("stringOne").innerHTML = tempOne;
 	document.getElementById("stringTwo").innerHTML = tempTwo;
