86179734fccfebb197446da868942f3b50ba4eec
Dakota Chambers <dakotachambers@gmail.com>
2014-09-06 13:14:36 -0500

    in a basic working state
    The script is in a basic working state. Need to fix grammar and add more
    buzzword options in the generator.

--- hackgen.js
+++ hackgen.js
@@ -1,11 +1,11 @@
 //js file that contains the string arrays
 //and the random string generator.
 
-var stringOne = ["test1", "test2", "test3", "a", "b", "c"];
-var stringTwo = ["4", "5", "6", "t", "y", "u"];
+var stringOne = ["Virtual Reality", "Cloud-based", "Open Source", "Gamified", "Adaptive", "Social", "Distuptive"];
+var stringTwo = ["Social Networking", "Enterprise", "Android", "iOS", "Windows", "Mac", "Linux", "Windows Phone", "Web", "Artificial Intelligence", "Gen-X"];
 
 function generateRands() {
-	document.getElementById("stringOne").innerHTML = stringOne[Math.floor((Math.random() * 6))];
-	document.getElementById("stringTwo").innerHTML = stringTwo[Math.floor((Math.random() * 6))];
+	document.getElementById("stringOne").innerHTML = stringOne[Math.floor((Math.random() * stringOne.length))];
+	document.getElementById("stringTwo").innerHTML = stringTwo[Math.floor((Math.random() * stringTwo.length))];
 
 }
