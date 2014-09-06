793e0d4ce5aecea39cd3595a5db1462046cba930
Dakota Chambers <dakotachambers@gmail.com>
2014-09-06 13:36:30 -0500

    changes to js, index.html
    added button to regenerate strings, added more string possibilities.

--- hackgen.js
+++ hackgen.js
@@ -1,11 +1,19 @@
 //js file that contains the string arrays
 //and the random string generator.
 
-var stringOne = ["Virtual Reality", "Cloud-based", "Open Source", "Gamified", "Adaptive", "Social", "Distuptive"];
-var stringTwo = ["Social Networking", "Enterprise", "Android", "iOS", "Windows", "Mac", "Linux", "Windows Phone", "Web", "Artificial Intelligence", "Gen-X"];
+var stringOne = ["Virtual Reality", "Cloud-based", "Open Source",
+ "Gamified", "Adaptive", "Social", "Distuptive", "Automated", "Back-end", "Front-end",
+ "Collaborative", "Scalable"];
+var stringTwo = ["Social Networking", "Enterprise", "Android", "iOS",
+ "Windows", "Mac", "Linux", "Windows Phone", "Web", "Artificial Intelligence", "Gen-X"
+ , "Big Data", "Solution", "E-commerce", "Big Data", "Networking"];
 
 function generateRands() {
-	document.getElementById("stringOne").innerHTML = stringOne[Math.floor((Math.random() * stringOne.length))];
-	document.getElementById("stringTwo").innerHTML = stringTwo[Math.floor((Math.random() * stringTwo.length))];
+
+	var tempOne = stringOne[Math.floor((Math.random() * stringOne.length))];
+	var tempTwo = stringTwo[Math.floor((Math.random() * stringTwo.length))];
+
+	document.getElementById("stringOne").innerHTML = tempOne;
+	document.getElementById("stringTwo").innerHTML = tempTwo;
 
 }
